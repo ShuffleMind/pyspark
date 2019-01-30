a4e9a2f34ed78565c0808c1b1c2a73a41cf2db67
Rafael Lucas Chagas <tqi_rchagas@uolinc.com>
2019-01-30 09:53:28 -0200

    bug fix

--- taxiRides.py
+++ taxiRides.py
@@ -39,17 +39,21 @@ def createParquet():
 
 def payment_method():
     #metodo para criacao de dimensao de tipo de pagamentos
+    dfPayment = spark.read.option("header","true")\
+                .option("inferschema","true")\
+                .parquet("/home/jovyan/NY_TAXI_RIDES")
     print('Criando dimensao de tipo de pagamento ')
     dfPayment = dfPayment\
                 .withColumn("Payment_des", functions\
-                .when(dfPayment.Payment_type == 1, 'CREDIT CARD')
-                .when(dfPayment.Payment_type == 2, 'CASH')
-                .when(dfPayment.Payment_type == 3, 'NO CHARGE')
-                .when(dfPayment.Payment_type == 4, 'DISPUTE')
-                .when(dfPayment.Payment_type == 5, 'UNKNOWN')
+                .when(dfPayment.payment_type == 1, 'CREDIT CARD')
+                .when(dfPayment.payment_type == 2, 'CASH')
+                .when(dfPayment.payment_type == 3, 'NO CHARGE')
+                .when(dfPayment.payment_type == 4, 'DISPUTE')
+                .when(dfPayment.payment_type == 5, 'UNKNOWN')
                 .otherwise('VOIDED TRIP')
                 )
-    dfPayment.write.parquet('/home/jovyan/NY_TAXI_RIDES'+'PAYMENT')
+    df_payment_type =  dfPayment.select("payment_type","Payment_des").distinct()
+    df_payment_type.write.parquet('/home/jovyan/NY_TAXI_RIDES_'+'PAYMENT_TYPE')
           
 if __name__ == '__main__':
     #fazendo a chamada do metodo de conversao dos dados para parquet
